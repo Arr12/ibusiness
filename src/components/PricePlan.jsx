0043b892e2d746bf831f695ef9a079e1dd960c0f
Arr12 <aryo.puruhito12@gmail.com>
2022-12-09 18:54:33 +0700

    (+) update pricelist plan

--- src/components/PricePlan.jsx
+++ src/components/PricePlan.jsx
@@ -18,10 +18,10 @@ function PricePlan() {
               </div>
             </div>
             <div className="flex flex-col w-[14rem] px-7 py-3">
-              <h2 className="font-bold text-xl">Intro</h2>
+              <h2 className="font-bold text-xl">Free</h2>
               <div className="font-bold text-gray-700 flex flex-row items-center mb-3">
-                <p className="font-bold text-2xl">$25 &nbsp;</p>
-                <small className="text-gray-200">/Month</small>
+                <p className="font-bold text-2xl">Rp 0,- &nbsp;</p>
+                <small className="text-gray-200">/Bulan</small>
               </div>
               <a
                 className="bg-blue-50 mt-3 text-blue-500 w-32 px-4 py-2 font-bold"
@@ -31,10 +31,10 @@ function PricePlan() {
               </a>
             </div>
             <div className="flex flex-col w-[14rem] px-7 py-3">
-              <h2 className="font-bold text-xl">Base</h2>
+              <h2 className="font-bold text-xl">Silver</h2>
               <div className="font-bold text-gray-700 flex flex-row items-center mb-3">
-                <p className="font-bold text-2xl">$39 &nbsp;</p>
-                <small className="text-gray-200">/Month</small>
+                <p className="font-bold text-2xl">Rp 2.500.000,- &nbsp;</p>
+                <small className="text-gray-200">/Bulan</small>
               </div>
               <a
                 className="bg-blue-50 mt-3 text-blue-500 w-32 px-4 py-2 font-bold"
@@ -44,20 +44,20 @@ function PricePlan() {
               </a>
             </div>
             <div className="flex flex-col w-[14rem] px-7 py-10 bg-blue-600 rounded-tl-2xl rounded-tr-2xl">
-              <h2 className="font-bold text-xl text-white">Popular</h2>
+              <h2 className="font-bold text-xl text-white">Gold</h2>
               <div className="font-bold text-gray-700 flex flex-row items-center mb-3">
-                <p className="font-bold text-white text-2xl">$99 &nbsp;</p>
-                <small className="text-gray-200">/Month</small>
+                <p className="font-bold text-white text-2xl">Rp 5.500.000,- &nbsp;</p>
+                <small className="text-gray-200">/Bulan</small>
               </div>
               <a className="hidden" href="#">
                 Choose Plan
               </a>
             </div>
             <div className="flex flex-col w-[14rem] px-7 py-3">
-              <h2 className="font-bold text-xl">Enterprise</h2>
+              <h2 className="font-bold text-xl">Platinum</h2>
               <div className="font-bold text-gray-700 flex flex-row items-center mb-3">
-                <p className="font-bold text-2xl">$199 &nbsp;</p>
-                <small className="text-gray-200">/Month</small>
+                <p className="font-bold text-2xl">Rp 7.500.000,- &nbsp;</p>
+                <small className="text-gray-200">/Bulan</small>
               </div>
               <a
                 className="bg-blue-50 mt-3 text-blue-500 w-32 px-4 py-2 font-bold"
@@ -98,7 +98,7 @@ function PricePlan() {
           </div>
           <div className="flex flex-row border-b-2 w-[90.5rem] bg-blue-600 px-3">
             <div className="flex flex-col w-[23rem] bg-blue-600 p-5 rounded-lg justify-center text-white">
-              <p className="text-md mb-1 w-[23rem]"> Free 1 Year Domain</p>
+              <p className="text-md mb-1 w-[23rem]"> Free 1 Year Domain .com</p>
             </div>
             <div className="flex flex-col w-[13.7rem] px-6 py-7 bg-white justify-center">
               <FontAwesomeIcon
@@ -127,7 +127,7 @@ function PricePlan() {
           </div>
           <div className="flex flex-row border-b-2 w-[90.5rem] bg-blue-600 px-3">
             <div className="flex flex-col w-[23rem] bg-blue-600 p-5 rounded-lg justify-center text-white">
-              <p className="text-md mb-1 w-[23rem]">1 Year Domain</p>
+              <p className="text-md mb-1 w-[23rem]">Fatest Engine</p>
             </div>
             <div className="flex flex-col w-[13.7rem] px-6 py-7 bg-white justify-center">
               <FontAwesomeIcon
@@ -156,7 +156,7 @@ function PricePlan() {
           </div>
           <div className="flex flex-row border-b-2 w-[90.5rem] rounded-bl-2xl rounded-br-2xl bg-blue-600 px-3 pb-3">
             <div className="flex flex-col w-[23rem] bg-blue-600 p-5 rounded-lg justify-center text-white">
-              <p className="text-md mb-1 w-[23rem]">Spaces</p>
+              <p className="text-md mb-1 w-[23rem]">Free Maintenance</p>
             </div>
             <div className="flex flex-col w-[13.7rem] px-6 py-7 bg-white justify-center rounded-bl-2xl">
               <FontAwesomeIcon
